600fda4fe87268ce709be49e39be7e16bd87ad2d
Thanh Đạt <101380464+PThDat@users.noreply.github.com>
2022-03-21 12:07:05 +0700

    Update Stack.cpp

--- Stack.cpp
+++ Stack.cpp
@@ -1,33 +1,42 @@
 template <class T>
 class Stack
 {
-	T data[1000];
+	T data[100];
 
 public:
 	int Size = 0;
 
-	void Push(const T& newData)
-	{
-		if (Size <= (sizeof(data) / sizeof(T)))
-		{
-			data[Size] = newData;
-			Size++;
-		}
-	}
-
-	void Pop()
-	{
-		if (Size > 0)
-			Size--;
-	}
-
-	T Top()
-	{
-		return data[Size - 1];
-	}
-
-	bool IsEmpty()
-	{
-		return Size == 0;
-	}
+	void Push(const T&);
+	void Pop();
+	T Top();
+	bool IsEmpty();
 };
+
+template <class T>
+void Stack<T>::Push(const T& newData)
+{
+	if (Size <= (sizeof(data) / sizeof(T)))
+	{
+		data[Size] = newData;
+		Size++;
+	}
+}
+
+template <class T>
+void Stack<T>::Pop()
+{
+	if (Size > 0)
+		Size--;
+}
+
+template <class T>
+T Stack<T>::Top()
+{
+	return data[Size - 1];
+}
+
+template <class T>
+bool Stack<T>::IsEmpty()
+{
+	return Size == 0;
+}
